7f20bdef722ce2524b554f14c1c6a8b0f61e21e7
kjhe1234 <kjhe1234@naver.com>
2024-04-29 15:42:13 +0900

    자바 깃허브 업로드 테스트중

---
+++ src/test_20240429/github_upload_test.java
@@ -0,0 +1,12 @@
+package test_20240429;
+
+public class github_upload_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Helloworld!!");
+		
+	}
+
+}
